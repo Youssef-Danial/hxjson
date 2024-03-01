b2773e4406287fc56d3afb3f57dcc96efe5d40d5
Youssef-Danial <64543206+Youssef-Danial@users.noreply.github.com>
2024-03-01 18:00:02 +0200

    Update hxjson.js

--- hxjson.js
+++ hxjson.js
@@ -6,11 +6,9 @@ function access_object(object, key) {
   }
   document.addEventListener("htmx:afterRequest", function (e) {
     if (e.srcElement.hasAttribute("hxjson")) {
-      //searching the target for jsondata
       response_data = JSON.parse(e.detail.xhr.response);
       console.log(response_data);
       var jsondata_list = e.detail.target.querySelectorAll("[jsondata]");
-      var object_list = {};
       for (const element of jsondata_list) {
         element.innerHTML = access_object(
           response_data,
