d057e3f43cd686cf2766bbf8ccd5f9f9cf14aac0
Youssef-Danial <64543206+Youssef-Danial@users.noreply.github.com>
2024-03-02 13:15:50 +0200

    Update hxjson.js

--- hxjson.js
+++ hxjson.js
@@ -1,18 +1,25 @@
 function access_object(object, key) {
-    for (const element of key.split(".")) {
-      object = object[element];
-    }
-    return object;
+  for (const element of key.split(".")) {
+    object = object[element];
   }
-  document.addEventListener("htmx:afterRequest", function (e) {
-    if (e.srcElement.hasAttribute("hxjson")) {
-      response_data = JSON.parse(e.detail.xhr.response);
-      var jsondata_list = e.detail.target.querySelectorAll("[jsondata]");
-      for (const element of jsondata_list) {
-        element.innerHTML = access_object(
-          response_data,
-          element.attributes.jsondata.nodeValue
-        );
+  return object;
+}
+document.addEventListener("htmx:afterRequest", function (e) {
+  if (e.detail.xhr.status == 404) {
+    // htmx will show error details
+    return;
+  }
+  if (e.srcElement.hasAttribute("hxjson")) {
+    //searching the target for jsondata
+    response_data = JSON.parse(e.detail.xhr.response);
+    var jsondata_list = e.detail.target.querySelectorAll("[jsondata]");
+    for (const element of jsondata_list) {
+      if (element.hasAttribute("unsafe")) {
+        element.textContent = access_object(response_data, element.attributes.jsondata.nodeValue)
+      } else {
+        element.innerHTML =
+          access_object(response_data, element.attributes.jsondata.nodeValue)
       }
     }
-  });
+  }
+});
